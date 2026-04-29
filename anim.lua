bae82141c90a0908c015adaabb92fb4c6d343647
WalterRalts <74102494+WalterRalts@users.noreply.github.com>
2026-04-28 22:58:47 -0400

    You can dance!

--- anim.lua
+++ anim.lua
@@ -3,29 +3,38 @@ local idle_time = 0
 local healpart = 0
 local blink_time = 0
 local blink_signal = math.random(30, 250)
-models.Walterv5.ralts.body.torso.chest.header:setParentType("Head")
 
 local mainPage = action_wheel:newPage("Basic")
 local hold = 0
 action_wheel:setPage(mainPage)
 
 function events.ENTITY_INIT()
-ActionActive = false
+      ActionActive = false
 end
 
 local wave = mainPage:newAction()
 wave:setTitle("Say hello!")
 wave:setItem("minecraft:stick")
 wave:onLeftClick(function()
-animations.Walterv5.ground_holding_idle:stop()
-animations.Walterv5.ground_idle:stop()
-idle_time = 0
-ActionActive = true
-animations.Walterv5.wave:play()
-animations.Walterv5.talk:play()
-sounds:playSound("WalterGreet", player:getPos(), 1, 1, false)
+      animations.Walterv5.ground_holding_idle:stop()
+      animations.Walterv5.ground_idle:stop()
+      idle_time = 0
+      ActionActive = true
+      animations.Walterv5.wave:play()
+      animations.Walterv5.talk:play()
+      sounds:playSound("WalterGreet", player:getPos(), 1, 1, false)
 end)
 
+local dance = mainPage:newAction()
+dance:setTitle("Shimmy")
+dance:setItem("minecraft:music_disc_cat")
+dance:onLeftClick(function()
+      animations.Walterv5.ground_holding_idle:stop()
+      animations.Walterv5.ground_idle:stop()
+      idle_time = 0
+      DancingActive = true
+      animations.Walterv5.dance1:play()
+end)
 
 local part_hat = models.Walterv5.ralts.body.torso.chest.header.Hat
 local hatonoff = mainPage:newAction()
@@ -50,55 +59,37 @@ function events.tick()
             bag:setVisible(false)
       end
 
-      local hats = player:getNbt()["fabric:attachments"]["accessories:inventory_holder"]["accessories_containers"]["hat"]["items"]
-      local hats2 = player:getNbt()["cardinal_components"]["trinkets:trinkets"]["head"]["hat"]["Items"]
-      if not (hats == nil and hats2 == nil) then
-            if hats[1] and hats[1]["id"]:find("hat") or hats2[1] and hats2[1]["id"] and hats2[1]["id"]:find("hat") then
-                  if part_hat:getVisible() then
-                        vanilla_model.HELMET_HEAD:setScale(0, 0, 0)
-                  else
-                        vanilla_model.HELMET_HEAD:setScale(1, 1, 1)
-                  end
-            end
-      end
-
       renderer:setEyeOffset(0, offset_cam, 0)
       nameplate.ENTITY:setPos(0, offset_cam, 0)
       renderer:setOffsetCameraPivot(0, offset_cam, 0)
-      if not ActionActive then
-            local crouching = player:getPose() == "CROUCHING"
-            local sprinting = player:isSprinting() and player:getVelocity().xz:length() >= 0.23
-            local blocking = player:isBlocking()
-            local fishing = player:isFishing()
-            local sleeping = player:getPose() == "SLEEPING"
-            local flying = player:getPose() == "FALL_FLYING"
-            local walking = player:getVelocity().xz:length() >= .01 and player:getVelocity().xz:length() < 0.23
-            local hungry = player:getFood() <= 7
-            local healing = player:getSaturation() > 0 and player:getHealth() < player:getMaxHealth() and player:getFood() > 18
-            local hand = player:isLeftHanded()
-            local holding_block = player:getHeldItem(hand):isBlockItem()
-            local holding_item = player:getHeldItem(hand) and not player:getHeldItem(hand):isBlockItem()
-            local shielding = player:isBlocking()
-            local charging = player:getActiveItem():getUseAction() == "BOW"
-            local nom = player:getActiveItem():getUseAction() == "EAT"
-            local jousting = player:getActiveItem():getUseAction() == "SPEAR"
-            local in_rain = player:isInRain()
-            local in_water = player:isInWater()
-            local mining = player:getSwingArm() ~= nil
-            local battle = player:getHeldItem(hand).id:find('sword') ~= nil
-            local swimming = player:getPose() == "SWIMMING"
-            --local riding = player:getVehicle() ~= nil
 
-            local charge_action = nom or jousting or charging
-            local leg_move = crouching or sprinting or walking or swimming
-            local idle = not leg_move and not charge_action and not mining and not fishing --and not riding
+      local crouching = player:getPose() == "CROUCHING"
+      local sprinting = player:isSprinting() and player:getVelocity().xz:length() >= 0.23
+      local blocking = player:isBlocking()
+      local fishing = player:isFishing()
+      local sleeping = player:getPose() == "SLEEPING"
+      local flying = player:getPose() == "FALL_FLYING"
+      local walking = player:getVelocity().xz:length() >= .01 and player:getVelocity().xz:length() < 0.23
+      local hungry = player:getFood() <= 7
+      local healing = player:getSaturation() > 0 and player:getHealth() < player:getMaxHealth() and player:getFood() > 18
+      local hand = player:isLeftHanded()
+      local holding_block = player:getHeldItem(hand):isBlockItem()
+      local holding_item = player:getHeldItem(hand) and not player:getHeldItem(hand):isBlockItem()
+      local shielding = player:isBlocking()
+      local charging = player:getActiveItem():getUseAction() == "BOW"
+      local nom = player:getActiveItem():getUseAction() == "EAT"
+      local jousting = player:getActiveItem():getUseAction() == "SPEAR"
+      local in_rain = player:isInRain()
+      local in_water = player:isInWater()
+      local mining = player:getSwingArm() ~= nil
+      local battle = player:getHeldItem(hand).id:find('sword') ~= nil
+      local swimming = player:getPose() == "SWIMMING"
+      --local riding = player:getVehicle() ~= nil
 
-            if crouching then
-                  models.Walterv5.ralts.body.torso.chest.header:setParentType("None")
-            else
-                  models.Walterv5.ralts.body.torso.chest.header:setParentType("Head")
-            end
-
+      local charge_action = nom or jousting or charging
+      local leg_move = crouching or sprinting or walking or swimming
+      local idle = not leg_move and not charge_action and not mining and not fishing --and not riding
+      if not ActionActive and not DancingActive then
             animations.Walterv5.ground_holding_idle:setPlaying(holding_block and not battle and not swimming)
             animations.Walterv5.ground_holding_walk:setPlaying(holding_block and walking and not swimming)
             animations.Walterv5.ground_holding_run:setPlaying(holding_block and sprinting and not swimming)
@@ -150,7 +141,6 @@ function events.tick()
                               animations.Walterv5.quirk_idle:play()
                         end
                   end
-                  models.Walterv5.ralts.body.torso.chest.header:setParentType("Head")
             end
             if healing then
                   healpart = healpart + 1
@@ -163,9 +153,14 @@ function events.tick()
                   end
             end
       else
-
+            log(hold, walking)
             if hold < 58 then
                   hold = hold + 1
+                  if walking then
+                        animations.Walterv5.dance1:stop()
+                        DancingActive = false
+                        animations.Walterv5.ground_idle:play()
+                  end
             else
                   ActionActive = false
                   hold = 0
